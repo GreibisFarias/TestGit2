d516f7042ae0bc44b8c9735c867f1a1730154834
Gray Farias <greibisfabialfariaperez@gmail.com>
2025-04-25 04:22:52 -0500

    Initial Commit

---
+++ TestGit2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TestGit2
+//
+//  Created by Greibis Farias on 25/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestGit2/TestGit2App.swift
@@ -0,0 +1,17 @@
+//
+//  TestGit2App.swift
+//  TestGit2
+//
+//  Created by Greibis Farias on 25/4/25.
+//
+
+import SwiftUI
+
+@main
+struct TestGit2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
