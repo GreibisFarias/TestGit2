53a4718457ee1bc0e1fabe7ce1a49665a5f2fffb
GreibisFarias <153131030+GreibisFarias@users.noreply.github.com>
2025-04-25 04:42:05 -0500

    Ticket-456
    Cambio de tamano de imagen

--- TestGit2/ContentView.swift
+++ TestGit2/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
         VStack {
             Image(systemName: "heart.fill")
                 .resizable()
-                .frame(width: 100, height: 100)
+                .frame(width: 200, height: 200)
                 
         }
         .padding()
